12497caaadc4374cdd994096e8772d02a8de2b29
AliGitOps <2516786946@qq.com>
2025-04-17 10:15:22 +0800

    添加类优化代码

--- main.py
+++ main.py
@@ -80,14 +80,12 @@ def main_qq(server, Maintext):
         <qq账号>
         <你的机器人关键词>
     """
-    # with open("email.config", "r") as qq:
-    #     QQ_EMAIN = qq.readline().strip()
-    #     QQ_KEYWORD = qq.readline().strip()
     try:
         # 实例化类
         conf = Conf()
         # qq 账号
         QQ_EMAIN = conf.read_email("email", "key")
+        # qq密码
         QQ_KEYWORD = conf.read_email("email", "value")
 
         con = smtplib.SMTP_SSL('smtp.qq.com', 465)
@@ -99,14 +97,14 @@ def main_qq(server, Maintext):
         subject = Header(f'Python {server}', 'utf-8').encode()
         msg['Subject'] = subject
 
-        msg['From'] = f'{QQ_EMAIN}@qq.com <{QQ_EMAIN}@qq.com>'
+        msg['From'] = f'{QQ_EMAIN} <{QQ_EMAIN}>'
 
-        msg['To'] = f'{QQ_EMAIN}@qq.com'
+        msg['To'] = f'{QQ_EMAIN}'
 
         text = MIMEText(Maintext, 'plain', 'utf-8')
         msg.attach(text)
 
-        con.sendmail(f'{QQ_EMAIN}', f'{QQ_KEYWORD}', msg.as_string())
+        con.sendmail(f'{QQ_EMAIN}', f'{QQ_EMAIN}', msg.as_string())
 
         con.quit()
     except smtplib.SMTPServerDisconnected:
@@ -124,7 +122,7 @@ def get_cpu():
 
     cpu_usage = psutil.cpu_percent(interval=1)
 
-    if cpu_usage > 75:
+    if cpu_usage > 1:
         alarm_text = "CPU使用率使用率告警"
         cpu_text = f"CPU使用率告警: 请及时远程服务器进行处理: {ip_address}"
 
@@ -153,7 +151,7 @@ def get_mem():
     memory_info = psutil.virtual_memory()
     memory_usage = memory_info.percent
 
-    if memory_usage > 75:
+    if memory_usage > 1:
         alarm_text = "内存使用率使用率告警"
         mem_text = f"内存使用率告警: 请及时远程服务器进行处理: {ip_address}"
 
@@ -185,26 +183,8 @@ def file_time(create_dir, file_name, cm):
 if __name__ == "__main__":
     get_mem()
     get_cpu()
-    conf = Conf()
-    eqq = conf.read_email("email", "key")
-    print(eqq)
 
-    # try:
-    #     location_var = sys.argv[1]
-    #     if location_var == "-h":
-    #         """
-    #             钉钉使用方法
-    #         """
-    #         print("用法如下:")
-    #         print("    钉钉: 在 dingtalk.config 配置文件中添加 钉钉的 第一行添加钉钉的 token, 第二行添加关键词比如")
-    #         print("        https://oapi.dingtalk.com/robot/send?access_token=xxx")
-    #         print("        Python")
-    #         """
-    #             QQ使用方法
-    #         """
-    #         print("    QQ: 在 email.config 配置文件中添加 QQ的 第一行添加QQ的 账号, 第二行添加授权码比如")
-    #         print("        25167869xx")
-    #         print("        tkdqhaxmryqqxxx")
-    #         exit(0)
-    # except Exception as e:
+
+
+
 
